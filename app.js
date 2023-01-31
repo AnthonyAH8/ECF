f7b992bce83272a3478018fb3aab3a7496ae5760
Anthony <an.hayart@gmail.com>
2023-01-31 10:07:09 +0100

    header

---
+++ app.js
@@ -0,0 +1,57 @@
+// console.log('Hello World')
+
+// let button = document.querySelector('button')
+
+// button.addEventListener('click', () => {
+//     console.log('Salut JS')
+//     button.classList.toggle('button')
+// })
+
+// let input = document.querySelector('input')
+
+// input.addEventListener('pointerout', (e) => {
+//     console.log('toto', e.currentTarget.checked)
+// })
+
+// let select = document.querySelector('select')
+
+// select.addEventListener('change', (e) => {
+//     console.log('toto', e.currentTarget.value)
+// })
+
+// let recup = document.querySelector('#recup')
+// let valider = document.querySelector('#valider')
+// let message = document.querySelector('span')
+
+// valider.addEventListener('click', (e) =>{
+//     e.preventDefault();
+
+//     message.textContent = recup.value.toUpperCase()
+// })
+
+
+// let chaine = "Rickrolled titi"
+// console.log(chaine)
+
+// let prenom = 'toto'
+
+// let nouveauPrenom = 'Bonjour, ' + prenom
+// console.log(nouveauPrenom)
+
+// console.log(`Bonjour, je m'appelle ${prenom}`)
+
+// let a = 1
+// let b = 2
+
+// if(a == b){
+//     console.log(true)
+// } else{
+//     console.log(false)
+// }
+
+
+// function calcul (c, d){
+//     return c + d;
+// }
+
+// console.log(calcul(4,5))
