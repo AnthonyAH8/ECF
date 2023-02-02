d84f793c007dbc62a73ed1f5e8386fdb782a2e28
Anthony <an.hayart@gmail.com>
2023-02-02 08:35:39 +0100

    JS

--- app.js
+++ app.js
@@ -1,57 +1,71 @@
-// console.log('Hello World')
 
-// let button = document.querySelector('button')
+let orderNow = document.querySelector('.orderNow')
+let success = document.querySelector('orderSuccess')
+let ordered = document.querySelector('.livraison')
 
-// button.addEventListener('click', () => {
-//     console.log('Salut JS')
-//     button.classList.toggle('button')
-// })
+orderNow.addEventListener('click', (e) => {
+  e.preventDefault();
 
-// let input = document.querySelector('input')
+  ordered.textContent = ('Votre commande a été prise en compte')
+  ordered.classList.add('order')
 
-// input.addEventListener('pointerout', (e) => {
-//     console.log('toto', e.currentTarget.checked)
-// })
+})
 
-// let select = document.querySelector('select')
+let meeting = document.querySelector('.condiments')
+let meetChef = document.querySelector('.meet')
+let replace = document.querySelector('.replace')
+let iframe = '<iframe width="560" height="315" src="https://www.youtube.com/embed/_GuOjXYl5ew" title="YouTube video player" frameborder="0"; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>'
 
-// select.addEventListener('change', (e) => {
-//     console.log('toto', e.currentTarget.value)
-// })
+meetChef.addEventListener('click', () => {
+  replace.innerHTML = iframe
+  console.log(replace)
+})
 
-// let recup = document.querySelector('#recup')
-// let valider = document.querySelector('#valider')
-// let message = document.querySelector('span')
+let count = document.querySelector('.count')
+let counter = document.querySelector('.counter')
+let number = 5
 
-// valider.addEventListener('click', (e) =>{
-//     e.preventDefault();
-
-//     message.textContent = recup.value.toUpperCase()
-// })
-
-
-// let chaine = "Rickrolled titi"
-// console.log(chaine)
-
-// let prenom = 'toto'
-
-// let nouveauPrenom = 'Bonjour, ' + prenom
-// console.log(nouveauPrenom)
-
-// console.log(`Bonjour, je m'appelle ${prenom}`)
-
-// let a = 1
-// let b = 2
-
-// if(a == b){
-//     console.log(true)
-// } else{
-//     console.log(false)
-// }
+counter.addEventListener('click', () => {
+  number += 1
+  count.innerHTML = number
+})
 
 
-// function calcul (c, d){
-//     return c + d;
-// }
+let random = document.querySelector('.random')
+let number1 = document.querySelector('.number1')
+let number2 = document.querySelector('.number2')
+let number3 = document.querySelector('.number3')
 
-// console.log(calcul(4,5))
+random.addEventListener('click', () => {
+  number1.innerHTML = Math.ceil(Math.random() * ((99999 - 1) + 1))
+})
+
+random.addEventListener('click', () => {
+  number2.innerHTML = Math.ceil(Math.random() * ((99999 - 1) + 1))
+})
+
+random.addEventListener('click', () => {
+  number3.innerHTML = Math.ceil(Math.random() * ((99999 - 1) + 1))
+})
+
+let mail = document.querySelector('.writingMail')
+let valider = document.querySelector('.subscribe')
+
+valider.addEventListener('click', (e) => {
+
+  e.preventDefault();
+
+  if (!mail.value) {
+    return ''
+  } else {
+    return mail.value = 'Merci EMAIL'
+  }
+})
+    mail.style.color = '#6415ff'
+
+let burger = document.querySelector('.burger')
+let navbar2 = document.querySelector('.navbar2')
+
+burger.addEventListener('click', () => {
+  navbar2.classList.toggle('navbar2')
+})
